a274e329376bb409af629b27719a54e93b145e36
Lucas Bibiano dos Santos <painkiller778@gmail.com>
2012-08-18 12:10:00 -0300

    configuração inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteOlimp::Application.config.secret_token = 'fdeec92f42fcda7279f2234329cb80767c8e8e6944191473486db48dac437deecfe55df881b6a96e283ae780ba43d7cc0ba3d421d778ffc2ad9af24e437b92e9'
