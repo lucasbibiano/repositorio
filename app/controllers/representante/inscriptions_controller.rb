0a88fa9f067497fcccdcdceafe380a055416a405
painkiller778 <painkiller778@gmail.com>
2012-09-01 17:36:13 -0300

    Erro de css no windows consertado

--- app/controllers/representante/inscriptions_controller.rb
+++ app/controllers/representante/inscriptions_controller.rb
@@ -7,16 +7,6 @@ class Representante::InscriptionsController < ApplicationController
 		@competitions = Admin::Competition.all
 	end
 
-	def inscrever_atleta_competicao
-		@competetition = Competition.find(params[:competition_id])
-		@athlete = Atleta.find(params[:athlete_id])
-
-		if @athlete.can_join(@competetition) #detectar maximo aqui de atletas
-			@competicao.athletes << @atleta
-		end
-
-	end
-
 	def inscrever
 		@athletes = Representante::Athlete.where(
 			:organization_id => current_user.organization.id)
@@ -36,11 +26,6 @@ class Representante::InscriptionsController < ApplicationController
 	    end
 
 		respond_to do |format|
-
-			#@competition.athletes.where(
-			#	:organization_id => current_user.organization.id) do |atl|
-			#	atl.competitions.delete(@competition)
-			#end
 
 			@competition.athletes.delete_if {|atl| atl.organization.id == current_user.organization.id}
 
